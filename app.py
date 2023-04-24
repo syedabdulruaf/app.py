797bf31475c514fadc3a6c0648f7b4bf742e571b
syedabdulruaf <131706813+syedabdulruaf@users.noreply.github.com>
2023-04-24 21:35:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,53 @@
-import facebook
 import streamlit as st
+import sqlite3
+import pandas as pd
 
-# Get your App ID and App Secret from the Facebook Developers website
-app_id = 'YOUR_APP_ID'
-app_secret = 'YOUR_APP_SECRET'
+# Create database
+conn = sqlite3.connect('users.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS users
+             (username text, password text, score integer)''')
+conn.commit()
 
-# Create a Facebook Graph API object
-graph = facebook.GraphAPI(access_token=f'{app_id}|{app_secret}', version='3.0')
+# Signup page
+def signup():
+    st.title('Signup')
+    username = st.text_input('Enter username')
+    password = st.text_input('Enter password', type='password')
+    if st.button('Signup'):
+        c.execute("INSERT INTO users (username, password, score) VALUES (?, ?, ?)", (username, password, 0))
+        conn.commit()
+        st.success('Account created')
 
-# Get the user's profile picture and name
-profile = graph.get_object('me', fields='id,name,picture')
-st.image(profile['picture']['data']['url'])
-st.write(f'Name: {profile["name"]}')
+# Login page
+def login():
+    st.title('Login')
+    username = st.text_input('Enter username')
+    password = st.text_input('Enter password', type='password')
+    if st.button('Login'):
+        c.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+        if c.fetchone() is not None:
+            st.success('Logged in')
+        else:
+            st.error('Incorrect username or password')
 
-# Get the user's friends
-friends = graph.get_connections('me', 'friends')['data']
-st.write('Friends:')
-for friend in friends:
-    st.write(friend['name'])
+# Leaderboard page
+def leaderboard():
+    st.title('Leaderboard')
+    c.execute("SELECT username, score FROM users ORDER BY score DESC LIMIT 10")
+    data = c.fetchall()
+    df = pd.DataFrame(data, columns=['Username', 'Score'])
+    st.table(df)
+
+# Sidebar menu
+menu = ['Signup', 'Login', 'Leaderboard']
+choice = st.sidebar.selectbox('Select a page', menu)
+
+if choice == 'Signup':
+    signup()
+elif choice == 'Login':
+    login()
+elif choice == 'Leaderboard':
+    leaderboard()
+
+conn.close()
